4317c8c1a9284d1d5436fd6bc387395b402f3c86
AleksandarTanev <aleksander.tanev.51@gmail.com>
2020-03-25 03:13:51 +0200

    add first db model -> Member

---
+++ Data/Register.Data.Models/Member.cs
@@ -0,0 +1,16 @@
+﻿namespace Register.Data.Models
+{
+    using Register.Data.Common.Models;
+
+    public class Member : BaseDeletableModel<int>
+    {
+        public string FirstName { get; set; }
+
+        public string Surname { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PIN { get; set; }
+
+    }
+}
---
+++ Data/Register.Data/Migrations/20200325010953_AddMembersModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Register.Data.Migrations
+{
+    public partial class AddMembersModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    PIN = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_IsDeleted",
+                table: "Members",
+                column: "IsDeleted");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Members");
+        }
+    }
+}
